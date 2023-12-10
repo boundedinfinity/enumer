e12aac845368675ebdc61bd57de3ae72f6417204
Brad Babb <brad_babb@cable.comcast.com>
2023-12-10 16:39:20 -0600

    updates

--- model.go
+++ model.go
@@ -1,28 +1,28 @@
 package enumer
 
 type EnumData struct {
-	Type        string        `json:"type" yaml:"type"`
-	Struct      string        `json:"struct" yaml:"struct"`
-	Package     string        `json:"package" yaml:"package"`
-	InputPath   string        `json:"input-path" yaml:"input-path"`
-	OutputPath  string        `json:"output-path" yaml:"output-path"`
-	Desc        string        `json:"desc" yaml:"desc"`
-	Header      string        `json:"header" yaml:"header"`
-	HeaderFrom  string        `json:"header-from" yaml:"header-from"`
-	HeaderLines []string      `json:"header-lines" yaml:"header-lines"`
-	SkipFormat  bool          `json:"skip-format" yaml:"skip-format"`
-	Debug       bool          `json:"debug" yaml:"debug"`
-	Overwrite   bool          `json:"overwrite" yaml:"overwrite"`
-	Serialize   EnumSerialize `json:"serialize" yaml:"serialize"`
-	Values      []EnumValue   `json:"values" yaml:"values"`
+	Type        string        `json:"type,omitempty" yaml:"type,omitempty"`
+	Struct      string        `json:"struct,omitempty" yaml:"struct,omitempty"`
+	Package     string        `json:"package,omitempty" yaml:"package,omitempty"`
+	InputPath   string        `json:"input-path,omitempty" yaml:"input-path,omitempty"`
+	OutputPath  string        `json:"output-path,omitempty" yaml:"output-path,omitempty"`
+	Desc        string        `json:"desc" yaml,omitempty:"desc,omitempty"`
+	Header      string        `json:"header,omitempty" yaml:"header,omitempty"`
+	HeaderFrom  string        `json:"header-from,omitempty" yaml:"header-from,omitempty"`
+	HeaderLines []string      `json:"header-lines,omitempty" yaml:"header-lines,omitempty"`
+	SkipFormat  bool          `json:"skip-format,omitempty" yaml:"skip-format,omitempty"`
+	Debug       bool          `json:"debug,omitempty" yaml:"debug,omitempty"`
+	Overwrite   bool          `json:"overwrite,omitempty" yaml:"overwrite,omitempty"`
+	Serialize   EnumSerialize `json:"serialize,omitempty" yaml:"serialize,omitempty"`
+	Values      []EnumValue   `json:"values,omitempty" yaml:"values,omitempty"`
 }
 
 type EnumSerialize struct {
-	Type  string `json:"type" yaml:"type"`
-	Value string `json:"value" yaml:"value"`
+	Type  string `json:"type,omitempty" yaml:"type,omitempty"`
+	Value string `json:"value,omitempty" yaml:"value,omitempty"`
 }
 
 type EnumValue struct {
-	Name       string `json:"name" yaml:"name"`
-	Serialized string `json:"serialized" yaml:"serialized"`
+	Name       string `json:"name,omitempty" yaml:"name,omitempty"`
+	Serialized string `json:"serialized,omitempty" yaml:"serialized,omitempty"`
 }
